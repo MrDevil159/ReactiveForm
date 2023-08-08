eebfbd71a641a094961dc66e35b75f328f1df54d
Mehfooz Ahmed <mehfoozmaaz@gmail.com>
2023-08-08 17:01:46 +0530

    dk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
     this.signUpForm = new FormGroup({
       userData: new FormGroup({
         username: new FormControl(null, [Validators.required, this.forbiddenName.bind(this)]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
 
       gender: new FormControl('male'),
@@ -43,7 +43,7 @@ export class AppComponent implements OnInit {
     return null;
   }
 
-  forbiddenEmails(control: FormControl): Promise<any> | Observable <any>{
+  forbiddenEmails(control: AbstractControl): Promise<any> | Observable <any>{
     const promise = new Promise<any>((resolve, reject) => {
       setTimeout(()=> {
         if(control.value === 'test@test.com') {
@@ -51,8 +51,8 @@ export class AppComponent implements OnInit {
         } else {
           resolve(null);
         }
-      },1500)
-    })
+      },1500);
+    });
     return promise;
   }
 }
